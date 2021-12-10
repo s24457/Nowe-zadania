fe379ae8376bba95ce5166fba6602e161bc4d483
s24457 <92578049+s24457@users.noreply.github.com>
2021-12-10 12:18:46 +0100

    Create Zad 1.cpp

---
+++ Zad 1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  for(int i = 0; i < 2;i++){
+    cout << 1 <<" "<< 2 << " " << 3 << " " << 5 << " " << 7 << " " << 11 << " " << 13 << " " << 17 << " " << 19 << endl; 
+  }
+} 
