7abb5b64d758487c26a46fc2d556a330d79db2c1
s24457 <92578049+s24457@users.noreply.github.com>
2021-12-10 12:23:51 +0100

    Create Zad 2.cpp

---
+++ Zad 2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  string s1;
+  double d2;
+  int i3;
+
+  cin >> s1;
+  cin >> d2;
+  cin >> i3;
+
+cout << s1 << endl;
+cout << endl;
+cout << d2 << endl;
+cout << endl;
+cout << i3 << endl;
+cout << endl;
+} 
